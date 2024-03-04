0fb2d428b1f951c3bcdd2b24143c4b95f1e55372
FernandoGaribay <garibaycejaf@gmail.com>
2024-03-04 10:29:06 -0600

    + Segundero atomico

--- src/reloj/componentes/RelojSegundero.java
+++ src/reloj/componentes/RelojSegundero.java
@@ -12,80 +12,93 @@ import java.util.logging.Logger;
 import reloj.Reloj;
 
 public class RelojSegundero implements Runnable {
-
+    
     private final Reloj RELOJ;
     private final int DIAMETRO_RELOJ;
     private final int TAMANO_SEGUNDOS;
     private final int CENTRO_X;
     private final int CENTRO_Y;
-
+    
     private Thread hilo;
-
+    private float anguloActual;
+    private int delay;
+    private float periodo;
+    
     private BufferedImage segundero;
-
+    
     public RelojSegundero(Reloj RELOJ, int DIAMETRO_RELOJ, int TAMANO_SEGUNDOS) {
         this.RELOJ = RELOJ;
         this.DIAMETRO_RELOJ = DIAMETRO_RELOJ;
         this.TAMANO_SEGUNDOS = TAMANO_SEGUNDOS;
-
+        
         this.CENTRO_X = this.DIAMETRO_RELOJ / 2;
         this.CENTRO_Y = this.DIAMETRO_RELOJ / 2;
-
+        
         this.hilo = new Thread(this);
-
+        this.anguloActual = calcularAngulo(Calendar.getInstance().get(Calendar.SECOND));
+        this.setAtomico(false);
+        
         this.segundero = new BufferedImage(DIAMETRO_RELOJ, DIAMETRO_RELOJ, BufferedImage.TYPE_INT_ARGB);
-
+        
         this.hilo.start();
     }
-
+    
     @Override
     public void run() {
         while (true) {
-            this.segundero = dibujarSegundero();
+            this.anguloActual = avanzarAngulo(this.anguloActual);
+            this.segundero = dibujarSegundero(this.anguloActual);
             RELOJ.dibujarSegundero(segundero);
 
             // Delay
             try {
                 System.out.println("Segundero dibujado");
-                Thread.sleep(1000);
+                Thread.sleep(delay);
             } catch (InterruptedException ex) {
                 Logger.getLogger(RelojSegundero.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
-
-    public BufferedImage dibujarSegundero() {
+    
+    public float avanzarAngulo(float anguloActual) {
+        float gradosPorSegundo = 6.0f;
+        float gradosPorIntervalo = gradosPorSegundo * periodo; // avanzar 6 grados cada 100 ms
+        anguloActual += gradosPorIntervalo;
+        
+        if (anguloActual >= 360) {
+            anguloActual = 0;
+        }
+        return anguloActual;
+    }
+    
+    public BufferedImage dibujarSegundero(float angulo) {
         BufferedImage tempSegundero = new BufferedImage(DIAMETRO_RELOJ, DIAMETRO_RELOJ, BufferedImage.TYPE_INT_ARGB);
-
+        
         Graphics2D g2 = tempSegundero.createGraphics();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        int segundo = Calendar.getInstance().get(Calendar.SECOND);
-//        int segundo = 0;
-        float angulo = calcularAngulo(segundo);
-
+        
         Point puntaAjuga = calcularCoordenada(angulo, TAMANO_SEGUNDOS);
         Point colaAjuga = calcularCoordenada(angulo + 180, (int) (TAMANO_SEGUNDOS * 0.25));
         Point[] puntosCola = calcularPuntosCola(angulo, TAMANO_SEGUNDOS);
 
         // Dibujado en el buffer
         dibujarSegundero(g2, puntaAjuga, colaAjuga, puntosCola);
-
+        
         g2.dispose();
         return tempSegundero;
     }
-
+    
     private float calcularAngulo(int segundo) {
         return segundo * 6 - 90;
     }
-
+    
     private void dibujarSegundero(Graphics2D g2, Point puntaAjuga, Point colaAjuga, Point[] puntosCola) {
         g2.setColor(new Color(255, 120, 120));
         g2.setStroke(new BasicStroke(3));
-
+        
         g2.drawLine(CENTRO_X, CENTRO_Y, CENTRO_X + puntaAjuga.x, CENTRO_Y + puntaAjuga.y);
         g2.drawLine(CENTRO_X, CENTRO_Y, CENTRO_X + colaAjuga.x, CENTRO_Y + colaAjuga.y);
-
+        
         int[] xPoints = {CENTRO_X + puntosCola[0].x,
             CENTRO_X + puntosCola[1].x,
             CENTRO_X + puntosCola[2].x,
@@ -108,7 +121,7 @@ public class RelojSegundero implements Runnable {
 //        g2.drawLine(CENTRO_X, CENTRO_Y, xPoints[5], yPoints[5]);
         g2.fillPolygon(xPoints, yPoints, 6);
     }
-
+    
     private Point[] calcularPuntosCola(float angulo, int tamanio) {
         Point punto0Cola = calcularCoordenada(angulo + 180, (int) (tamanio * 0.07));
         Point punto1Cola = calcularCoordenada(angulo + 180 + 11, (int) (tamanio * 0.15));
@@ -116,14 +129,24 @@ public class RelojSegundero implements Runnable {
         Point punto3Cola = calcularCoordenada(angulo + 180 - 5, (int) (tamanio * 0.3));
         Point punto4Cola = calcularCoordenada(angulo + 180 - 11, (int) (tamanio * 0.15));
         Point punto5Cola = calcularCoordenada(angulo + 180, (int) (tamanio * 0.07));
-
+        
         return new Point[]{punto0Cola, punto1Cola, punto2Cola, punto3Cola, punto4Cola, punto5Cola};
     }
-
+    
     private Point calcularCoordenada(float angulo, int tamanio) {
         int x = (int) (tamanio * Math.cos(Math.toRadians(angulo)));
         int y = (int) (tamanio * Math.sin(Math.toRadians(angulo)));
-
+        
         return new Point(x, y);
+    }
+    
+    public void setAtomico(boolean atomico) {
+        if (atomico) {
+            this.delay = 100;
+            this.periodo = 0.1f;
+        } else {
+            this.delay = 1000;
+            this.periodo = 1.0f;
+        }
     }
 }
