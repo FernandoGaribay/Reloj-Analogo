e9c31bdbe2d2474663dfc5915564b01dc7ebd43f
FernandoGaribay <garibaycejaf@gmail.com>
2024-03-12 14:55:34 -0600

    + Seleccion del reloj

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,13 +6,17 @@ import relojOnDisplay.RelojOnDisplay;
 
 public class Main extends JFrame {
 
-    RelojOnDisplay objReloj;
-    
+    private String relojSeleccionado;
+    Reloj objRelojQuartz;
+    RelojOnDisplay objRelojOnDisplay;
+
     public Main() {
         initComponents();
-        
-        objReloj = new RelojOnDisplay(500, 500, false);
-        pnlContenedor.add(objReloj);
+
+        relojSeleccionado = "Quartz";
+        objRelojQuartz = new Reloj(500, 500, false);
+
+        pnlContenedor.add(objRelojQuartz);
         pnlContenedor.revalidate();
         pnlContenedor.repaint();
     }
@@ -23,6 +27,8 @@ public class Main extends JFrame {
 
         jPanel2 = new javax.swing.JPanel();
         toggleAtomico = new javax.swing.JToggleButton();
+        btnOnDisplay = new javax.swing.JButton();
+        btnQuartz = new javax.swing.JButton();
         background = new javax.swing.JPanel();
         pnlContenedor = new javax.swing.JPanel();
 
@@ -47,6 +53,26 @@ public class Main extends JFrame {
         });
         jPanel2.add(toggleAtomico, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 140, 40));
 
+        btnOnDisplay.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
+        btnOnDisplay.setText("On Display");
+        btnOnDisplay.setFocusPainted(false);
+        btnOnDisplay.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnOnDisplayActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnOnDisplay, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 10, -1, 40));
+
+        btnQuartz.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
+        btnQuartz.setText("Quartz");
+        btnQuartz.setFocusPainted(false);
+        btnQuartz.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnQuartzActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnQuartz, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 10, 95, 40));
+
         getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 500, 60));
 
         background.setBackground(new java.awt.Color(255, 255, 255));
@@ -64,12 +90,57 @@ public class Main extends JFrame {
 
     private void toggleAtomicoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_toggleAtomicoActionPerformed
         pnlContenedor.removeAll();
-        objReloj.pararReloj();
-        objReloj = new RelojOnDisplay(500, 500, toggleAtomico.isSelected());
-        pnlContenedor.add(objReloj);
+
+        switch (relojSeleccionado) {
+            case "Quartz":
+                objRelojQuartz.pararReloj();
+                objRelojQuartz = new Reloj(500, 500, toggleAtomico.isSelected());
+                pnlContenedor.add(objRelojQuartz);
+                break;
+            case "OnDisplay":
+                objRelojOnDisplay.pararReloj();
+                objRelojOnDisplay = new RelojOnDisplay(500, 500, toggleAtomico.isSelected());
+                pnlContenedor.add(objRelojOnDisplay);
+                break;
+            default:
+                throw new AssertionError();
+        }
+
         pnlContenedor.revalidate();
         pnlContenedor.repaint();
     }//GEN-LAST:event_toggleAtomicoActionPerformed
+
+    private void btnOnDisplayActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOnDisplayActionPerformed
+        if (!relojSeleccionado.equals("OnDisplay")) {
+            relojSeleccionado = "OnDisplay";
+
+            if (objRelojQuartz != null) {
+                objRelojQuartz.pararReloj();
+            }
+
+            pnlContenedor.removeAll();
+            objRelojOnDisplay = new RelojOnDisplay(500, 500, toggleAtomico.isSelected());
+            pnlContenedor.add(objRelojOnDisplay);
+            pnlContenedor.revalidate();
+            pnlContenedor.repaint();
+        }
+    }//GEN-LAST:event_btnOnDisplayActionPerformed
+
+    private void btnQuartzActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnQuartzActionPerformed
+        if (!relojSeleccionado.equals("Quartz")) {
+            relojSeleccionado = "Quartz";
+
+            if (objRelojOnDisplay != null) {
+                objRelojOnDisplay.pararReloj();
+            }
+
+            pnlContenedor.removeAll();
+            objRelojQuartz = new Reloj(500, 500, toggleAtomico.isSelected());
+            pnlContenedor.add(objRelojQuartz);
+            pnlContenedor.revalidate();
+            pnlContenedor.repaint();
+        }
+    }//GEN-LAST:event_btnQuartzActionPerformed
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -81,6 +152,8 @@ public class Main extends JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel background;
+    private javax.swing.JButton btnOnDisplay;
+    private javax.swing.JButton btnQuartz;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel pnlContenedor;
     private javax.swing.JToggleButton toggleAtomico;
